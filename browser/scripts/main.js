a78bd22bbd4cc08b53fca1c1f51f2379ed5249db
Dan Plyukhin <dplyukhin@gmail.com>
2013-10-13 01:48:19 -0400

    Updated peer.js for AMD support

--- browser/scripts/main.js
+++ browser/scripts/main.js
@@ -1,7 +1,7 @@
 require.config({
     paths: {
         jquery: '/lib/jquery/jquery',
-        peerjs: '/lib/peerjs_devel/dist/peer'
+        peerjs: '/lib/peerjs/dist/peer'
     }
 });
 
@@ -10,11 +10,11 @@ require(['jquery', 'connection'], function ($, connection) {
     window.PeerConnection = connection;
 
     $("#submit_local").click( function () {
-        local_id = $("#local_id").val();
+        var local_id = $("#local_id").val();
         connection.setLocalName(local_id);
         
         $("#submit_remote").click( function () {
-            remote_id = $("#remote_id").val();
+            var remote_id = $("#remote_id").val();
             connection.remoteConnect(remote_id);
         });
     }); 
